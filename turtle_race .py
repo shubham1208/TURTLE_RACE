1e1cddb30920e71a48f0ed5bb6db116cc271a9f2
Shubham Sahu <67262119+shubham1208@users.noreply.github.com>
2020-11-27 12:38:00 +0530

    Update turtle_race .py

--- turtle_race .py
+++ turtle_race .py
@@ -106,31 +106,42 @@ while shubham.xcor() <= 250 and jhanvi.xcor() <= 250 and samriddhi.xcor() <= 250
     samriddhi.forward(random.randint(1,10))
     riya.forward(random.randint(1,10))
     shreemoyee.forward(random.randint(1,10))
+    
 
 # CELEBRATE WIN
-if shubham.xcor() > jhanvi.xcor() and shubham.xcor() > samriddhi.xcor() and shubham.xcor() > shreemoyee.xcor() and shubham.xcor() > riya.xcor():
+if shubham.xcor() > jhanvi.xcor() and shubham.xcor() > samriddhi.xcor() and shubham.xcor() > shreemoyee.xcor() and shubham.xcor() > riya.xcor() :
     print("Shubham won!")
+    shubham.shapesize(outline = 4)
     for i in range(4):
         shubham.right(90)
+    shubham.shapesize(outline = 1)
     
 elif jhanvi.xcor() > shubham.xcor() and jhanvi.xcor() > samriddhi.xcor() and jhanvi.xcor() > shreemoyee.xcor() and jhanvi.xcor() > riya.xcor():
     print("Jhanvi won!")
+    jhanvi.shapesize(outline = 4)
     for j in range(4):
         jhanvi.right(90)
+    jhanvi.shapesize(outline = 4)
 
 elif samriddhi.xcor() > jhanvi.xcor() and samriddhi.xcor() > shubham.xcor() and samriddhi.xcor() > shreemoyee.xcor() and samriddhi.xcor() > riya.xcor():
     print("Samriddhi won!")
+    samriddhi.shapesize(outline = 4)
     for k in range(4):
         samriddhi.right(90)
+    samriddhi.shapesize(outline = 1)
 
-elif shreemoyee.xcor() > jhanvi.xcor() and shreemoyee.xcor() > samriddhi.xcor() and shreemoyee.xcor() > shubham.xcor() and shubham.xcor() > riya.xcor():
+elif shreemoyee.xcor() > jhanvi.xcor() and shreemoyee.xcor() > samriddhi.xcor() and shreemoyee.xcor() > shubham.xcor() and shreemoyee.xcor() > riya.xcor():
     print("Shreemoyee won!")
+    shreemoyee.shapesize(outline = 4)
     for l in range(4):
         shreemoyee.right(90)
+    shreemoyee.shapesize(outline = 1)
 
 elif riya.xcor() > jhanvi.xcor() and riya.xcor() > samriddhi.xcor() and riya.xcor() > shreemoyee.xcor() and riya.xcor() > shubham.xcor():
     print("Riya won!")
+    riya.shapesize(outline = 4)
     for m in range(4):
         riya.right(90)
+    riya.shapesize(outline = 1)
 
 turtle.done()
